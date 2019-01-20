02060b3afebfda8504a83b983e9f10df92f7bb90
Yusuf Can Celik <yusufcancelik@hotmail.com>
2019-01-21 02:50:30 +0300

    Added Category Actions

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _categoryService.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            return Ok(category);
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.Delete(id);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Category categoryModel)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category != null)
+            {
+                await _categoryService.Update(categoryModel);
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
